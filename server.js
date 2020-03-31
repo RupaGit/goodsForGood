e4921659b47997c8e6e2b44263a078a8e46ea2ed
Rupa Popuri <swarupakarnam@yahoo.com>
2020-03-30 20:34:55 -0400

    initial passport code added

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ const env = require("dotenv");
 const app = express();
 var PORT = process.env.PORT || 4000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+// app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/goodsForGood");
+
+// Start the API server
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
