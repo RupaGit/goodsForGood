374934a683bd88c373cebef6c57bbec0de4a1dc2
Rupa Popuri <spopuri@mediaocean.com>
2020-04-02 05:56:00 -0400

    Passport updates

---
+++ client/src/utils/API.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default {
+  // Sign up a user
+  signUp: function() {
+    return axios.post("/api/signUp", userData);
+  },
+  // Sign in a user
+  signIn: function() {
+    return axios.post("/api/signIn", userData);
+  }
+};
