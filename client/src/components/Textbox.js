c0b141186eee51ca3c129159320e04671f727f10
GuyYaffeErmoza <guyyaffear@gmail.com>
2020-04-02 08:43:38 -0400

    adding a TextBox

---
+++ client/src/components/Textbox.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Form, TextArea } from 'semantic-ui-react'
+
+const TextAreaSamantic = () => (
+  <Form>
+    <TextArea placeholder='working on the text' />
+  </Form>
+)
+
+export default TextAreaSamantic
